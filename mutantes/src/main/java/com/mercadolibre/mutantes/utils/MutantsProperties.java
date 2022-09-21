f1021be8c4800b501fc6704ebf83d8aec9ccdb4d
Brayden Lopera <b.lopera9505@gmail.com>
2022-09-21 07:40:06 -0500

    Solucion doble app

--- mutantes/src/main/java/com/mercadolibre/mutantes/utils/MutantsProperties.java
+++
@@ -1,6 +0,0 @@
-package com.mercadolibre.mutantes.utils;
-
-public class MutantsProperties {
-    public final static int MINIMUM_SEQUENCE_LENGTH = 4;
-    public final static int MINIMUM_NUMBER_OF_SEQUENCES = 2;
-}
--- src/main/java/com/mercadolibre/mutantes/MutantesApplication.java
+++ src/main/java/com/mercadolibre/mutantes/MutantesApplication.java
@@ -2,8 +2,10 @@ package com.mercadolibre.mutantes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.mercadolibre.mutantes.controllers,com.mercadolibre.mutantes.services,com.mercadolibre.mutantes.repositories,com.mercadolibre.mutantes.config")
 public class MutantesApplication {
 
 	public static void main(String[] args) {
